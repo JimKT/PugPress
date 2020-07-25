c300129738347f66e40f00d11fff9bbfd2f22128
Jamie Throup <jamiethroup@gmail.com>
2020-07-25 07:13:18 +0100

    Updates

---
+++ wp-content/themes/pugpress/components/favicons.php
@@ -0,0 +1,13 @@
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?=get_template_directory_uri()?>/dist/images/favicon/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" href="<?=get_template_directory_uri()?>/dist/images/favicon/favicon-32x32.png" sizes="32x32"/>
+<link rel="icon" type="image/png" href="<?=get_template_directory_uri()?>/dist/images/favicon/favicon-194x194.png" sizes="194x194"/>
+<link rel="icon" type="image/png" href="<?=get_template_directory_uri()?>/dist/images/favicon/android-chrome-192x192.png" sizes="192x192"/>
+<link rel="icon" type="image/png" href="<?=get_template_directory_uri()?>/dist/images/favicon/favicon-16x16.png" sizes="16x16"/>
+<link rel="manifest" href="<?=get_template_directory_uri()?>/dist/images/favicon/manifest.json"/>
+<link rel="mask-icon" href="<?=get_template_directory_uri()?>/dist/images/favicon/safari-pinned-tab.svg" color="#aa0004"/>
+<link rel="shortcut icon" href="<?=get_template_directory_uri()?>/dist/images/favicon/favicon.ico"/>
+<meta name="msapplication-TileColor" content="#1a1a1a"/>
+<meta name="msapplication-TileImage" content="<?=get_template_directory_uri()?>/dist/images/favicon/mstile-144x144.png"/>
+<meta name="msapplication-config" content="<?=get_template_directory_uri()?>/dist/images/favicon/browserconfig.xml"/>
+<meta name="theme-color" content="#ffffff"/>
--- wp-content/themes/pugpress/gulpfile.js
+++ wp-content/themes/pugpress/gulpfile.js
@@ -1,18 +1,15 @@
 "use strict";
 // Require the Following to Work
 const gulp = require('gulp');
-const runSequence = require('run-sequence');
 const replace = require('gulp-replace');
-const clean = require('gulp-clean');
-const rename = require('gulp-rename');
-const inlinesource = require('gulp-inline-source');
-const inlineCss = require('gulp-inline-css');
 const pug = require('gulp-pug');
 const imagemin = require('gulp-imagemin');
 const gap = require('gulp-append-prepend');
 const autoprefixer = require('gulp-autoprefixer');
 const ext_replace = require('gulp-ext-replace');
-const stylus = require('gulp-stylus');
+const cleancss = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const include = require('gulp-include');
 
 // Append and Prepend Wordpress Header and Footer
 gulp.task('tags', () => 
@@ -21,13 +18,18 @@ gulp.task('tags', () =>
     .pipe(gap.appendText('<?php get_footer(); ?>'))
     .pipe(gulp.dest('./'))
 );
-// Compile Stylus 
-gulp.task('stylus', () =>
-	gulp.src('./src/css/main.styl')
-	.pipe(stylus({
+// Compile sass 
+gulp.task('sass', () =>
+	gulp.src('./src/sass/main.scss')
+	.pipe(sass({
 		compress: true
 	}))
-	.pipe(gulp.dest('assets/css'))
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+		cascade: false
+	}))
+    .pipe(cleancss({compatibility: 'ie8'}))
+	.pipe(gulp.dest('dist/css'))
 );
 // Compile PUG Files
 gulp.task('php', () =>
@@ -37,7 +39,7 @@ gulp.task('php', () =>
 		}))
 		.pipe(replace('&lt;', '<'))
 		.pipe(replace('&gt;', '>'))
-		.pipe(replace( /(["'])assets\//g, '$1<?=get_template_directory_uri()?>/assets/' ) )
+		.pipe(replace( /(["'])assets\//g, '$1<?=get_template_directory_uri()?>/dist/' ) )
 		.pipe(ext_replace('.php'))
 		.pipe(gulp.dest('./'))
 );
@@ -49,44 +51,44 @@ gulp.task('functions', () =>
 // Move Javascript to Assets
 gulp.task('javascript', () =>
 	gulp.src('./src/js/main.js')
-		.pipe(gulp.dest('assets/js'))
+		.pipe(include())
+			.on('error', console.log)
+		.pipe(gulp.dest('dist/js'))
 );
 // Image Minification
 gulp.task('images', () =>
 	gulp.src('./src/images/**/*')
-		.pipe(imagemin({
-			optimizationLevel: 10
-		}))
-		.pipe(gulp.dest('assets/images'))
+	.pipe(imagemin([
+		imagemin.gifsicle({interlaced: true}),
+		imagemin.mozjpeg({quality: 75, progressive: true}),
+		imagemin.optipng({optimizationLevel: 5}),
+		imagemin.svgo({
+			plugins: [
+				{removeViewBox: true},
+				{cleanupIDs: false}
+			]
+		})
+	]))
+	.pipe(gulp.dest('dist/images'))
 );
 // Run ALL Tasks
-gulp.task('build', () =>
-	runSequence('images', 'stylus', 'php', 'functions', 'javascript', 'tags')
-);
-// On stylus Change
-gulp.task('watch-stylus', () =>
-	runSequence('stylus')
-);
+gulp.task('build', gulp.series('images', 'sass', 'php', 'functions', 'javascript', 'tags'));
+// On sass Change
+gulp.task('watch-sass', gulp.series('sass'));
 // On Javascript Change
-gulp.task('watch-javascript', () =>
-	runSequence('javascript')
-);
+gulp.task('watch-javascript', gulp.series('javascript'));
 // On Image Change
-gulp.task('images', () =>
-	runSequence('images')
-);
+gulp.task('images', gulp.series('images'));
 // On Template Change
-gulp.task('templates', () =>
-	runSequence('php', 'functions', 'tags',)
-);
+gulp.task('templates', gulp.series('php', 'functions', 'tags'));
 // Default ( Runs all Tasks on Watch )
 gulp.task('default', function () {
-	// On stylus Change
-	gulp.watch(['./src/css/**/*'], ['watch-stylus']);
+	// On sass Change
+	gulp.watch(['./src/sass/**/*'], gulp.series('watch-sass'));
 	// On Javascript Change
-	gulp.watch(['./src/js/**/*'], ['javascript']);
+	gulp.watch(['./src/js/**/*'], gulp.series('javascript'));
 	// On Image Change
-	gulp.watch(['./src/images/**/*'], ['images']);
+	gulp.watch(['./src/images/**/*'], gulp.series('images'));
 	// On Template Change
-	gulp.watch(['./src/templates/**/*'], ['templates']);
+	gulp.watch(['./src/templates/**/*'], gulp.series('templates'));
 });
